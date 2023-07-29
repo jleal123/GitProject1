917b107dd17c0a0f4ffb09b7e4818ed74f55d27e
Joshua Leal <Joshua Leal@JoshPC>
2023-07-29 10:34:15 -0400

    implemented subtract()

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -5,16 +5,18 @@ public class DemoDay2 {
 	public static void main(String[] args) {
 
 		System.out.println("Test");
-		
-		
-		
+
 	}
 
 	
-	//add method
-	
+	//add method	
 	public static void add(int a, int b) {
 		System.out.println(a+b);
+	}
+	
+	//subtract method
+	public static void subtract(int a, int b) {
+		System.out.println(a-b);
 	}
 	
 }
