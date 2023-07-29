bad44275889358de6b4bfd29b461f06f4d9436a3
jleal123 <140639277+jleal123@users.noreply.github.com>
2023-07-29 10:49:35 -0400

    Update DemoDay2.java

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -18,5 +18,15 @@ public class DemoDay2 {
 	public static void subtract(int a, int b) {
 		System.out.println(a-b);
 	}
+
+	//Yasir
+	public static void divide(int a, int b) {
+		System.out.println(a/b);
+	}
+
+	//add method	
+	public static void add(int a, int b, int c) {
+		System.out.println(a+b+c);
+	}
 	
 }
